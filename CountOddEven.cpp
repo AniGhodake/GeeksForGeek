d450350ce6ef9bd78991793ee73230b9510f63ee
AniketGhodake <ghodakeaniket27@gmail.com>
2024-06-12 13:04:06 +0530

    12th June 2024

---
+++ CountOddEven.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    /*Function to count even and odd elements in the array
+    * arr : Array with its elements
+    * sizeof_array : number of array elements
+    * countOdd : variable to count number of odd elements
+    * countEven : variable to count number of even elements
+    */
+    vector<int> countOddEven(int arr[], int sizeof_array)
+    {
+        vector<int>ans;
+        int countOdd = 0;
+        int countEven = 0;
+       for(int i = 0; i< sizeof_array; i++){
+           if((arr[i] ^1) == (arr[i] +1)){
+               countEven++;
+           }
+           else{
+               countOdd++;
+           }
+       }
+       ans.push_back(countOdd);
+       ans.push_back(countEven);
+       return ans;
+       
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	
+	// Testcase input
+	int testcase;
+	cin >> testcase;
+	
+	while(testcase--){
+	    int sizeof_array;
+	    cin >> sizeof_array;
+	    
+	    int arr[sizeof_array];
+	    
+	    // Array elements input
+	    for(int i = 0; i < sizeof_array; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    // Calling function to count even and odd
+	    vector<int> res = ob.countOddEven(arr, sizeof_array);
+	    for(auto t:res)
+	    {
+	        cout<<t<<" ";
+	    }
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
