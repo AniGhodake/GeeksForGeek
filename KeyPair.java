b9c9e0ce563264c951f19f9736fa84b4b659e3ec
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-01-22 23:08:21 +0530

    22nd January 2025

---
+++ KeyPair.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            // int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[0]);
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            boolean ans = new Solution().twoSum(arr, x);
+            System.out.println(ans ? "true" : "false");
+            // System.out.println("~");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean twoSum(int arr[], int target) {
+       Arrays.sort(arr);
+       int n = arr.length;
+       
+       int  s = 0;
+       int  e = n-1;
+       while(s<e){
+           if(arr[s]+arr[e] == target){
+               return true;
+           }
+           else if(arr[s]+arr[e]< target){
+               s++;
+           }
+           else{
+               e--;
+           }
+       }
+       
+       return false;
+       
+       
+    }
+}
