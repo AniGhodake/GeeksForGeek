554aa61938c19e96318ea5e8bd4dfa11928ab320
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-02-02 20:57:37 +0530

    2nd Februray 2025

---
+++ NthNodeFromEnd_LinkedList.java
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/* Structure of node
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+} */
+
+class Solution {
+
+    // Function to find the data of kth node from
+    // the end of a linked list.
+    int count = 0;
+    int result = -1;
+    int getKthFromLast(Node head, int k) {
+        // Your code here
+        findKth(head, k);
+        return result;
+    }
+    
+    void findKth(Node node, int k){
+        if(node == null){
+            return;
+        }
+        
+        findKth(node.next, k);
+        
+        count++;
+        
+        if(count == k){
+            result = node.data;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//{ Driver Code Starts.
+
+public class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            if (!input.isEmpty()) {
+                String[] numbers = input.split("\\s+");
+                for (String num : numbers) {
+                    if (!num.isEmpty()) {
+                        arr.add(Integer.parseInt(num));
+                    }
+                }
+            }
+
+            int x = Integer.parseInt(br.readLine().trim());
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); ++i) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+
+            Solution g = new Solution();
+            // System.out.println(k);
+            System.out.println(g.getKthFromLast(head, x));
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
