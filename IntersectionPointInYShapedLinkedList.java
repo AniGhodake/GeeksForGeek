dd0dc1b1f29e5ab1bc1344af6ea3bd76909ccd28
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-02-05 21:21:35 +0530

    5th February 2025

---
+++ IntersectionPointInYShapedLinkedList.java
@@ -0,0 +1,160 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedList_Intersection {
+    Node head = null;
+    Node tail = null;
+
+    public void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            tail = head;
+        } else {
+            // Node temp = head;
+            // while (temp.next != null)
+            // temp = temp.next;
+
+            // temp.next = node;
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    /* Function to print linked list */
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Driver program to test above functions */
+    public static void main(String args[]) throws IOException {
+
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t > 0) {
+
+            LinkedList_Intersection llist1 = new LinkedList_Intersection();
+            LinkedList_Intersection llist2 = new LinkedList_Intersection();
+            LinkedList_Intersection llist3 = new LinkedList_Intersection();
+
+            String str[] = read.readLine().trim().split(" ");
+            int n1 = str.length;
+            Node head1 = new Node(Integer.parseInt(str[0]));
+
+            Node tail1 = head1;
+
+            for (int i = 1; i < n1; i++) {
+                int a = Integer.parseInt(str[i]);
+                tail1.next = (new Node(a));
+                tail1 = tail1.next;
+            }
+
+            str = read.readLine().trim().split(" ");
+            Node head2 = new Node(Integer.parseInt(str[0]));
+            Node tail2 = head2;
+            int n2 = str.length;
+            for (int i = 1; i < n2; i++) {
+                int b = Integer.parseInt(str[i]);
+                tail2.next = (new Node(b));
+                tail2 = tail2.next;
+            }
+
+            str = read.readLine().trim().split(" ");
+            int n3 = str.length;
+            if (n3 > 0) {
+
+                Node head3 = new Node(Integer.parseInt(str[0]));
+                tail1.next = head3;
+                tail2.next = head3;
+                Node tail3 = head3;
+                for (int i = 1; i < n3; i++) {
+                    int c = Integer.parseInt(str[i]);
+                    tail3.next = (new Node(c));
+                    tail3 = tail3.next;
+                }
+            }
+            Intersect obj = new Intersect();
+            Node intersectNode =
+                obj.intersectPoint(head1, head2); // Get the intersection node
+            System.out.println(intersectNode.data);
+            System.out.println("~");
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}*/
+
+class Intersect {
+    // Function to find intersection point in Y shaped Linked Lists.
+    static Node intersectPoint(Node head1, Node head2) {
+        Node a = head1;
+        Node b = head2;
+        
+        while(a != null && b != null){
+            if(a == b){
+                return a;
+            }
+            
+            a = a.next;
+            b = b.next;
+        }
+        
+        if(a == null){ // which means that B is bigger in size
+            int blen = 0;
+            while(b != null){
+                blen++;
+                b = b.next;
+            }
+            
+            while(blen-- >0){
+                head1 = head1.next;
+            }
+        } 
+        else{
+            int alen = 0;
+            while(a != null){
+                alen++;
+                a = a.next;
+            }
+            
+            while(alen-- >0){
+                head2 = head2.next;
+            }
+        }
+    
+        
+    return head1;
+    }
+}
