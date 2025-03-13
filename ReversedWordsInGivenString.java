8676bef6912fb075a4d631248dc38d51889e5ccd
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-03-13 23:19:34 +0530

    13th March 2025

---
+++ ReversedWordsInGivenString.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt(); // Read the number of test cases
+        sc.nextLine();        // Consume the newline character after reading the integer
+
+        while (t-- > 0) {
+            String s = sc.nextLine(); // Read the input string
+
+            // Remove surrounding quotes from the string
+            String str = s.substring(1, s.length() - 1);
+
+            Solution obj = new Solution(); // Create an object of the Solution class
+            String ans = obj.reverseWords(str); // Reverse the words in the string
+
+            // Print the result with surrounding quotes
+            System.out.println("\"" + ans + "\"");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to reverse words in a given string
+    public String reverseWords(String s) {
+       s = s.trim();
+       
+       String words[] = s.split("\\s+");
+       
+       StringBuilder reversed = new StringBuilder();
+       
+       for(int i = words.length -1; i >= 0; i--){
+           reversed.append(words[i]);
+           if(i > 0){
+               reversed.append(" ");
+           }
+       }
+       
+       return reversed.toString();
+    }
+}
