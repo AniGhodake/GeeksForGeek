b65cb1473463dfec8532eddb4a8634a8adb8b387
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-03-25 21:36:12 +0530

    25th March 2025

---
+++ ParenthesisChecker.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
+            String st = sc.next();
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    static boolean isBalanced(String s) {
+        Stack<Character>st = new Stack<>();
+        
+        for(char ch : s.toCharArray()){
+            if(ch =='(' || ch =='{' || ch =='['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                
+                char top = st.pop();
+                 if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false; // Mismatched brackets
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
